67a08fe93f0001f3d752f703db76132ba4e43f2b
LAvA <paulin.weidmann@gmail.com>
2026-05-08 19:08:41 +0200

    Update sections.js

--- sections.js
+++ sections.js
@@ -1,1 +1,15 @@
+const headers = document.querySelectorAll(
+  ".main-header, .sub-header"
+);
 
+headers.forEach(header => {
+
+  header.addEventListener("click", () => {
+
+    const section = header.parentElement;
+
+    section.classList.toggle("open");
+
+  });
+
+});
