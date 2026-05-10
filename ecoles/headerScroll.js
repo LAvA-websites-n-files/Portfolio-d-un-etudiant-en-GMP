b3f6a566df098b3be3ca3edd405fc1e87d93b014
LAvA <paulin.weidmann@gmail.com>
2026-05-10 20:01:58 +0200

    Update headerScroll.js

--- ecoles/headerScroll.js
+++ ecoles/headerScroll.js
@@ -1,9 +1,5 @@
 const gradient = document.querySelector(".header-gradient");
 
-window.addEventListener("scroll", () => {
-
-  const scroll = window.scrollY;
-
   // déplacement progressif
   const move = Math.min(scroll * 0.35, window.innerHeight * 0.55);
 
@@ -14,17 +10,13 @@ const intro = document.querySelector(".intro-text");
 
 window.addEventListener("scroll", () => {
   const rect = intro.getBoundingClientRect();
-
-  // distance entre le haut de l'écran et l'élément
   const windowHeight = window.innerHeight;
 
-  // progression basée sur l'entrée dans l'écran
   let progress = 1 - (rect.top / windowHeight);
-
   progress = Math.min(Math.max(progress, 0), 1);
 
   const scale = 1 - progress * 0.35;
-  const translateY = -progress * 120;
+  const translateY = -progress * 80;
   const opacity = 1 - progress;
   const blur = progress * 6;
 
