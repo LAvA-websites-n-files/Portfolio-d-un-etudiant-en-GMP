a5b466bc85d530de50c978a0a13908236b52db1b
LAvA <paulin.weidmann@gmail.com>
2026-05-10 17:41:31 +0200

    Update headerScroll.js

--- ecoles/headerScroll.js
+++ ecoles/headerScroll.js
@@ -4,15 +4,9 @@ window.addEventListener("scroll", () => {
 
   const scroll = window.scrollY;
 
-  // limite la progression
-  const maxScroll = window.innerHeight;
+  // vitesse du dégradé
+  const move = Math.min(scroll * 0.8, window.innerHeight * 0.6);
 
-  // progression entre 0 et 1
-  const progress = Math.min(scroll / maxScroll, 1);
-
-  // le dégradé monte progressivement
-  const newHeight = 25 + (progress * 75);
-
-  gradient.style.height = `${newHeight}%`;
+  gradient.style.transform = `translateY(-${move}px)`;
 
 });
